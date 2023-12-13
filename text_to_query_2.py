b8ddad6a11b51f862de9ed3c366965f99070f6bb
unknown <eric77711@gmail.com>
2023-12-13 19:55:18 +0100

    prompt with top_k

--- text_to_query_2.py
+++ text_to_query_2.py
@@ -24,7 +24,7 @@ def question_to_query(question):
     )
 
     sqlite_prompt = """You are a SQLite expert. Given an input question, first create a syntactically correct SQLite query to answer the question.
-    You can order the results to return the most informative data in the database.
+    Unless the user specifies in the question a specific number of examples to obtain, query for at most {top_k} results using the LIMIT clause as per SQLite. You can order the results to return the most informative data in the database.
     Never query for all columns from a table. You must query only the columns that are needed to answer the question. Wrap each column name in backticks (`) to denote them as delimited identifiers.
     Pay attention to use only the column names you can see in the tables below. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
     Pay attention to use date('now') function to get the current date, if the question involves "today".
@@ -41,7 +41,7 @@ def question_to_query(question):
         example_prompt=example_prompt,
         prefix=sqlite_prompt,
         suffix=PROMPT_SUFFIX,
-        input_variables=["input", "table_info"], #These variables are used in the prefix and suffix
+        input_variables=["input", "table_info", "top_k"], #These variables are used in the prefix and suffix
     )
 
     llm = GooglePalm(google_api_key=os.environ["api_key"], temperature=0)
